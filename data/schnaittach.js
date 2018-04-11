a04484a3dce062cc2d81062818c83f57b5f17e7e
markus-bb <38273312+markus-bb@users.noreply.github.com>
2018-04-11 21:50:41 +0200

    Add files via upload

---
+++ data/schnaittach.js
@@ -0,0 +1,219 @@
+{
+  "type": "FeatureCollection",
+  "generator": "overpass-ide",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2018-04-11T16:04:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/51849765",
+        "addr:city": "Neunkirchen a. Brand",
+        "addr:housenumber": "1",
+        "addr:postcode": "91077",
+        "addr:street": "Baad",
+        "addr:suburb": "Baad",
+        "building": "yes",
+        "name": "Biolandhof Schmidt",
+        "organic": "only",
+        "shop": "farm"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.1323291,
+              49.6204253
+            ],
+            [
+              11.1324664,
+              49.6202767
+            ],
+            [
+              11.1326151,
+              49.6203345
+            ],
+            [
+              11.1324778,
+              49.620483
+            ],
+            [
+              11.1323291,
+              49.6204253
+            ]
+          ]
+        ]
+      },
+      "id": "way/51849765"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/105253020",
+        "addr:city": "Gräfenberg",
+        "addr:country": "DE",
+        "addr:housenumber": "4",
+        "addr:place": "Lilling",
+        "addr:postcode": "91322",
+        "addr:suburb": "Lilling",
+        "building": "yes",
+        "craft": "distillery",
+        "name": "Frienhof in Hirtengärtl / Genusshof Pingold",
+        "phone": "+49 9192 7494",
+        "shop": "farm",
+        "smoking": "no",
+        "tourism": "guest_house",
+        "website": "http://pingold.com"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.2878569,
+              49.6353245
+            ],
+            [
+              11.2880238,
+              49.6352105
+            ],
+            [
+              11.2881285,
+              49.6352749
+            ],
+            [
+              11.2879615,
+              49.6353888
+            ],
+            [
+              11.2878569,
+              49.6353245
+            ]
+          ]
+        ]
+      },
+      "id": "way/105253020"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "way/123272708",
+        "addr:city": "Kirchensittenbach",
+        "addr:country": "DE",
+        "addr:housenumber": "1",
+        "addr:postcode": "91241",
+        "addr:street": "Wallsdorf",
+        "building": "yes",
+        "email": "info@reimehof.de",
+        "fax": "+49 9152 928681",
+        "name": "Reimehof",
+        "note": "bewirtschaftet nach den Richtlinien des Bioland Verbandes",
+        "operator": "Fam. Beyer",
+        "organic": "only",
+        "phone": "+49 9152 9287963",
+        "shop": "farm",
+        "website": "http://reimehof.de"
+      },
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              11.4411648,
+              49.6046375
+            ],
+            [
+              11.4411102,
+              49.6046032
+            ],
+            [
+              11.4410314,
+              49.6045536
+            ],
+            [
+              11.4409491,
+              49.6045019
+            ],
+            [
+              11.4410502,
+              49.6044344
+            ],
+            [
+              11.4412659,
+              49.60457
+            ],
+            [
+              11.4411648,
+              49.6046375
+            ]
+          ]
+        ]
+      },
+      "id": "way/123272708"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/456498015",
+        "name": "Beerenland",
+        "note": "Verschiedene Obstarten selbst zu pflücken",
+        "shop": "farm"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          11.2357977,
+          49.4915395
+        ]
+      },
+      "id": "node/456498015"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/3319884874",
+        "addr:city": "Igensdorf",
+        "addr:country": "DE",
+        "addr:housenumber": "25",
+        "addr:postcode": "91338",
+        "addr:street": "Forchheimer Straße",
+        "craft": "distillery",
+        "name": "Brennerei Fahner",
+        "shop": "farm",
+        "website": "http://www.hofladen-fahner.de/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          11.2274498,
+          49.6231966
+        ]
+      },
+      "id": "node/3319884874"
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "@id": "node/3320028712",
+        "addr:city": "Leutenbach",
+        "addr:country": "DE",
+        "addr:housenumber": "41",
+        "addr:place": "Mittelehrenbach",
+        "addr:postcode": "91359",
+        "craft": "distillery",
+        "name": "Bio-Hof Wilibald Schmidt",
+        "phone": "09199 1804",
+        "shop": "farm"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          11.1723273,
+          49.6893155
+        ]
+      },
+      "id": "node/3320028712"
+    }
+  ]
+}
