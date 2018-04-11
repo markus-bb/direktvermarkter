31cee5a46f02397e6e503bc7553afa3b790c290f
markus-bb <38273312+markus-bb@users.noreply.github.com>
2018-04-11 22:14:12 +0200

    Update schnaittach.js
    erst mal "karlsruhe" - jetzt müssten die  Daten in Schnaittach erscheinen...

--- data/schnaittach.js
+++ data/schnaittach.js
@@ -1,4 +1,4 @@
-var schnaittach = {
+var karlsruhe = {
   "type": "FeatureCollection",
   "generator": "overpass-ide",
   "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
